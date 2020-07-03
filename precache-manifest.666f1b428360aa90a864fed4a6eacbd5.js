ad2b79cf07517085f1f7024dcf8fb52bf2a6c7a3
shradhakatyal <skatyal2204@gmail.com>
2020-07-03 15:07:19 +0530

    Updates

---
+++ precache-manifest.666f1b428360aa90a864fed4a6eacbd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4881e9744ad4197cd388c26f6002cc20",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89ad7864e8c0855076f2",
+    "url": "/static/css/main.874d0a3d.chunk.css"
+  },
+  {
+    "revision": "abf73c06bd67c72cde07",
+    "url": "/static/js/2.c7204ee4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c7204ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89ad7864e8c0855076f2",
+    "url": "/static/js/main.ffed55e1.chunk.js"
+  },
+  {
+    "revision": "97ec32cacca735b5aacd",
+    "url": "/static/js/runtime-main.b044faef.js"
+  }
+]);
