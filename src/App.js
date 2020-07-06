76a06be014afb4caff546c0b012b1d0960e52e09
shradhakatyal <skatyal2204@gmail.com>
2020-07-06 21:53:50 +0530

    local storage integrated

--- src/App.js
+++ src/App.js
@@ -7,23 +7,27 @@ import TodoForm from './Components/TodoForm/TodoForm';
 
 const App = () => {
   // useState gives us two things - the value and a function that modifies that value. It can be thought of as this.state and this.setState in class based components.
-  const [todos, setTodos ] = useState([]);
+  const storedTodos = localStorage.getItem('todos');
+  const [todos, setTodos ] = useState(storedTodos && JSON.parse(storedTodos).length ? JSON.parse(storedTodos) : []);
 
   const addTodo = text => {
     const newTodos = [...todos, { text }];
     setTodos(newTodos);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
   }
 
   const completeTodo = index => {
     const newTodos = [...todos];
     newTodos[index].isCompleted = true;
     setTodos(newTodos);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
   }
 
   const deleteTodo = index => {
     const newTodos = [...todos];
     newTodos.splice(index, 1);
     setTodos(newTodos);
+    localStorage.setItem('todos', JSON.stringify(newTodos));
   }
 
   return (
